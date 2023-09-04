171f218a05999709432ac21223d3f5349ec2f343
YuriyOhii <uaogiy@gmail.com>
2023-09-04 18:57:03 +0300

    puts feedback in different components

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,6 +1,47 @@
-import {Feedback} from "./Feedback"
-export const App = () => {
-  return (
-    <Feedback/>
-  );
-};
+import { Component } from 'react';
+
+import { Section } from './Section';
+import { FeedbackOptions } from './FeedbackOptions';
+import { Statistics } from './Statistics';
+
+export class App extends Component {
+  state = {
+    good: 0,
+    neutral: 0,
+    bad: 0,
+  };
+
+  handleClick = ({ target }) => {
+    const btnName = target.name;
+    this.setState(prevState => ({
+      ...prevState,
+      [btnName]: prevState[btnName] + 1,
+    }));
+  };
+
+  countTotalFeedback = () => {
+    const total = Object.values(this.state);
+    return total.reduce((acc, el) => el + acc, 0);
+  };
+
+  countPositiveFeedbackPercentage = () => {
+    const { good } = this.state;
+    return Math.floor((100 * good) / this.countTotalFeedback());
+  };
+
+  render() {
+    const { good, neutral, bad } = this.state;
+    return (
+      <Section title="Please leave feedback">
+        <FeedbackOptions onLeaveFeedback={this.handleClick} options={Object.keys(this.state)} />
+        <Statistics
+          good={good}
+          neutral={neutral}
+          bad={bad}
+          total={this.countTotalFeedback()}
+          positivePercentage={this.countPositiveFeedbackPercentage()}
+        />
+      </Section>
+    );
+  }
+}
